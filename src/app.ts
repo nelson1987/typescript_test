a2f780d441d8135bdfbb8959bac54e86989ff4bc
Nelson Neto <nelson.ash@outlook.com>
2025-01-06 17:08:07 +0000

    change tests

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,75 @@
-export const sum = (a: number, b: number) => {
+import { z, ZodIssue } from 'zod';
+export type ValidationError = {
+  [key: string]: string[];
+};
+export type ValidationResponse = {
+  isValid: boolean;
+  data?: unknown;
+  error?: ValidationError | Error;
+};
+export const FailResult = (error: ValidationError): ValidationResponse => {
+  return {
+    isValid: false,
+    error: error,
+  };
+};
+export const ExceptionResult = (error: Error): ValidationResponse => {
+  return {
+    isValid: false,
+    error: error,
+  };
+};
+export const SuccessResult = (data: unknown): ValidationResponse => {
+  return {
+    isValid: true,
+    data: data,
+  };
+};
+
+export type UserBio = {
+  userName: string;
+  bio: string;
+};
+const UserBioSchema = z.object({
+  userName: z.string(), //{
+  //     required_error: 'Nome é obrigatório',
+  //     invalid_type_error: 'Nome deve ser uma string',
+  //   })
+  //   .min(3, 'Nome deve ter no mínimo 3 caracteres')
+  //   .max(50, 'Nome deve ter no máximo 50 caracteres'),
+  bio: z
+    .string()
+    .min(25, 'Bio must be at least 25 characters long')
+    .max(120, 'Bio must not expected 120 characters'),
+});
+export const formatError = (errors: ZodIssue[]): ValidationError => {
+  return errors.reduce((acc: { [key: string]: string[] }, curr) => {
+    const path = curr.path[0] as string;
+    if (!acc[path]) {
+      acc[path] = [];
+    }
+    acc[path].push(curr.message);
+    return acc;
+  }, {});
+};
+export const handler = (userBio: UserBio): ValidationResponse => {
   try {
-    throw new Error("Oops");
-    //return a + b;
+    const parsedUserBio = UserBioSchema.safeParse(userBio);
+    if (parsedUserBio.success) {
+      return SuccessResult(parsedUserBio.data);
+    } else {
+      const errors = formatError(parsedUserBio.error.errors);
+      return FailResult(errors);
+    }
   } catch (error) {
-    reportError(error.message);
+    return ExceptionResult(error);
   }
 };
-const reportError = (message: string) => {};
-export type Response = {
-  message: string;
-  isValid: boolean;
+
+export const verificar = (userBio: number): string => {
+  try {
+    return `user é ${userBio}`;
+  } catch (error) {
+    return `erro`;
+  }
 };
-export const errorResponse = (messageResponse: string) => {
-  return { message: messageResponse, isValid: false };
-};
-// export const somar = (a: number, b: number): number => {
-//   return a + b;
-// };
-// export const sum = (a: string, b: number): string => `${b}${a}`;
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,7 +1,67 @@
-import { sum } from "../src/app";
+import { isValid } from 'zod';
+import { handler, UserBio } from '../src/app';
 
-describe("Suite", () => {
-  it("should throw Oops", () => {
-    expect(() => sum(1, 5)).toThrow("Oops");
+const userBio: UserBio = {
+  userName: 'ABCD',
+  bio: "I'm John Doe, a Web developer and a Tech writer.",
+};
+
+describe('Handler', () => {
+  it('Validation Pass', () => {
+    expect(handler(userBio)).toEqual({
+      data: {
+        bio: "I'm John Doe, a Web developer and a Tech writer.",
+        userName: 'ABCD',
+      },
+      isValid: true,
+    });
+  });
+  it('Validation Error', () => {
+    const userBio: UserBio = {
+      userName: '',
+      bio: 'I'.repeat(24),
+    };
+    expect(handler(userBio)).toEqual({
+      error: { bio: ['Bio must be at least 25 characters long'] },
+      isValid: false,
+    });
+  });
+  it('should return ExceptionResult when exceptions tart', () => {
+    const result = handler(null);
+    expect(result).toEqual({
+      error: { bio: ['Bio must not expected 120 characters'] },
+      isValid: false,
+    });
+  });
+});
+describe('Validation', () => {
+  it('Validation Pass', () => {
+    expect(handler(userBio)).toEqual({
+      data: {
+        bio: "I'm John Doe, a Web developer and a Tech writer.",
+        userName: 'ABCD',
+      },
+      isValid: true,
+    });
+  });
+  it('Validation Bio must be at least 25 characters long', () => {
+    const userBio: UserBio = {
+      userName: '',
+      bio: 'I'.repeat(24),
+    };
+    expect(handler(userBio)).toEqual({
+      error: { bio: ['Bio must be at least 25 characters long'] },
+      isValid: false,
+    });
+  });
+  it('Validation Bio must not expected 120 characters', () => {
+    const userBio: UserBio = {
+      userName: '',
+      bio: 'I'.repeat(121),
+    };
+    expect(handler(userBio)).toEqual({
+      error: { bio: ['Bio must not expected 120 characters'] },
+      isValid: false,
+    });
   });
 });
