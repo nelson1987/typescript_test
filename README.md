359dc9e7f079ae2038b08dbc4ba3d6a64f9ad334
nelson.neto <nelson.neto@aarin.com.br>
2025-01-06 09:07:18 -0300

    alteracao para verificacao em testes

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ https://danywalls.com/how-to-test-errors-with-jest-in-typescript
 https://www.typescriptlang.org/docs/handbook/declaration-files/do-s-and-don-ts.html
 
 https://engineering.udacity.com/handling-errors-like-a-pro-in-typescript-d7a314ad4991
+
+https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
-export const sum = (a: number, b: number): string => {
-  throw new Error("Oops");
-  //return a + b;
+export const sum = (a: number, b: number) => {
+  try {
+    throw new Error("Oops");
+    //return a + b;
+  } catch (error) {
+    reportError(error.message);
+  }
+};
+const reportError = (message: string) => {};
+export type Response = {
+  message: string;
+  isValid: boolean;
+};
+export const errorResponse = (messageResponse: string) => {
+  return { message: messageResponse, isValid: false };
 };
 // export const somar = (a: number, b: number): number => {
 //   return a + b;
